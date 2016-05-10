8792e606d8841d61af5f381150442823ed3ad86d
BriOD <briodonoghue@gmail.com>
2016-05-09 23:28:37 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+      # write your code here!
+      pigeons = {}
+    
+      data.each do |characteristics, vals|
+            vals.each do |characteristic, names|
+                  names.each do |name|
+                        (pigeons[name] ||= {})[characteristics] ||= []
+                        pigeons[name][characteristics] << characteristic.to_s
+                  end
+                end
+          end
+      pigeons
 end
